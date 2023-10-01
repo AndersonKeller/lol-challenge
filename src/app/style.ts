8986eda85b3565f5c35924b7cd45321638fc8779
Anderson Keller <andersonkellervendas@hotmail.com>
2023-09-30 21:11:50 -0300

    config colors and imgs backups

---
+++ src/app/style.ts
@@ -0,0 +1,9 @@
+import tw from "tailwind-styled-components";
+
+export const MainStyled = tw.main`
+  flex
+  bg-black       
+  h-full
+ 
+  text-yellow-600
+`;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      textColor: {
+        "yellow-600": "#f4c258",
+        "yellow-700": "#c89b3c",
+        "blue-600": "#0866B0",
+        "cyan-400": "#46CAE9",
+        "gray-50": "#F9FAFB",
+        "gray-100": "#F3F4F6",
+        "gray-200": "#E5E7EB",
+        "gray-300": "#D1D5DB",
+        "gray-400": "#9CA3AF",
+        "gray-500": "#6B7280",
+        "gray-600": "#4B5563",
+        "gray-700": "#374151",
+        "gray-800": "#1F2937",
+        "gray-900": "#090D15",
+        "gray-950": "#030712",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
